30d74733f854e95e03f68cf5ee49ac9ebd39b61f
FatimaAlmashi <f.almashi@windowslive.com>
2019-02-21 15:48:25 +0300

    test

--- task_21/settings.py
+++ task_21/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tnvhv(22@t1#1!f9$&6gl#0-&9(symuxb$=x_2h6zkg*tdi4iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '138.68.65.5']
 
 
 # Application definition
@@ -81,12 +81,24 @@ WSGI_APPLICATION = 'task_21.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		}
 	}
-}
+else:
+	DATABASES = {
+	    'default': {
+	        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	        'NAME': 'django',
+	        'USER': 'django',
+	        'PASSWORD': '6b0ce6cdb8c53656431746d11af090c2',
+	        'HOST': 'localhost',
+	        'PORT': '',
+	    }
+	}
 
 
 # Password validation
